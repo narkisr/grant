46c6ae73ea4012ea4e584c069da0e449c2741e0d
ronen <narkisr@gmail.com>
2020-06-04 23:40:16 +1000

    process now returns the same format as the input for emit

--- src/grant/parse.clj
+++ src/grant/parse.clj
@@ -1,6 +1,7 @@
 (ns grant.parse
   "Parsing sudoer files"
   (:require
+   [clojure.core.match :refer [match]]
    [clojure.walk :as w]
    [instaparse.core :as insta]))
 
@@ -10,47 +11,60 @@
 (defn sudoers [f]
   ((insta/parser "resources/sudo.ebnf" :input-format :ebnf :auto-whitespace whitespace) f))
 
-; Converting parser result to nested maps and seqs
-
-
-(defn key-pair? [v]
-  (and (sequential? v) (= (count v) 2) (keyword? (first v)) (string? (second v))))
-
-(defn key-map? [v]
-  (and (sequential? v) (= (count v) 2) (keyword? (first v)) (map? (second v))))
-
-(defn key-list? [v]
-  (and (sequential? v) (keyword? (first v)) (sequential? (second v))))
-
-(defn key-maps? [v]
-  (and (sequential? v) (keyword? (first v)) (sequential? (rest v)) (every? map? (rest v))))
-
-(defn unique-map-keys? [ms]
-  (letfn  [(count-keys [acc m] (-> acc (update :sum + (count (keys m))) (update :uniq (fn [v] (into v (set (keys m)))))))]
-    (let [{:keys [sum uniq]} (reduce count-keys {:sum 0 :uniq #{}} ms)]
-      (= sum (count uniq)))))
-
-(defn key-mergable-maps?
-  "Merging k to maps sequence only if the keys in the map are unique (the merge will not override the keys)"
-  [v]
-  (and (sequential? v) (keyword? (first v)) (sequential? (rest v)) (every? map? (rest v)) (unique-map-keys? (rest v))))
-
-(defn not-commandname [v]
-  (not (= (first v) :commandname)))
-
-(defn process [output]
+(defn defaults [d]
   (w/postwalk
    (fn [v]
-     (cond
-       (key-pair? v) (apply hash-map v)
-       (and (key-map? v) (not-commandname v)) {(first v) (second v)}
-       (and (key-mergable-maps? v) (not-commandname v)) {(first v) (apply merge (rest v))}
-       (key-maps? v) {(first v) (rest v)}
-       (key-list? v) {(first v) (rest v)}
+     (match [v]
+       [[:default-entry parameters]]  [:default parameters]
+       [[:default-entry [:default-type [:host-list [:host [:hostname hostname]]]] parameters]]  [:default/servers hostname parameters]
+       [[:default-entry [:default-type [:runas-list [:runas-member [:user user]]]] parameters]]  [:default/runas user parameters]
+       [[:default-entry [:default-type [:alias-name alias-name]] parameters]]  [:default/user-alias alias-name parameters]
+       [[:default-entry [:default-type [:user user]] parameters]]  [:default/user user parameters]
+       [[:default-entry [:default-type [:cmnd-list [:cmnd [:alias-name alias-name]]]] parameters]]  [:default/cmnd-alias alias-name parameters]
+       [[:parameter-list & parameters]]  parameters
+       [[:parameter "!" identifier]]  [:not identifier]
+       [[:parameter identifier value]]  [identifier value]
+       [[:parameter identifier]]  [identifier]
+       :else v)) d))
+
+(defn cmnd-alias [a]
+  (w/postwalk
+   (fn [v]
+     (match [v]
+       [[:cmnd-alias [:alias-name alias-name] [:cmnd-list & cmnd-list]]] [:cmnd-alias alias-name cmnd-list]
+       [[:cmnd [:commandname & commands]]] commands
+       :else v)) a))
+
+(defn user-spec [u]
+  (w/postwalk
+   (fn [v]
+     (match [v]
+       [[:user-spec [:user-list & users] [:host-list & hosts] [:cmnd-spec-list & commands]]] [:user-spec users hosts commands]
+       [[:cmnd-spec spec]] spec
+       [[:cmnd-spec & spec]] spec
+       [[:runas-spec [:runas-list [:runas-member [:alias-name alias-name]]]]] [:runas [:alias alias-name]]
+       [[:tag-spec tag]] [:tag tag]
+       [[:cmnd [:alias-name alias-name]]] [:alias-name alias-name]
+       [[:cmnd [:commandname & commands]]] commands
+       :else v)) u))
+
+(defn process
+  "Converting parser result to grant simplified form "
+  [output]
+  (w/postwalk
+   (fn [v]
+     (match [v]
+       [[:default-entry _]] (defaults v)
+       [[:default-entry & _]] (defaults v)
+       [[:cmnd-alias & _]] (cmnd-alias v)
+       [[:user-spec & _]] (user-spec v)
        :else v)) output))
 
 (comment
-  (apply disj #{1 2} #{2})
-  (key-mergable-maps?)
+  (process (sudoers (slurp "test/resources/defaults")))
+  (process (sudoers (slurp "test/resources/aliases")))
+  (process (sudoers (slurp "test/resources/single-line-no-passwd")))
+  (process (sudoers (slurp "test/resources/combined")))
   (sudoers (slurp "test/resources/defaults"))
-  (sudoers (slurp "test/resources/aliases")))
+  (sudoers (slurp "test/resources/aliases"))
+  (sudoers (slurp "test/resources/single-line-no-passwd")))
