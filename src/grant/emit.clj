1b21dfce1d9e5b24bf25efb7a0606e8a199df287
ronen <narkisr@gmail.com>
2020-06-04 23:39:04 +1000

    fixing default emit and adding more cases

--- src/grant/emit.clj
+++ src/grant/emit.clj
@@ -11,30 +11,33 @@
   (w/postwalk
    (fn [v]
      (match [v]
-       [[:default/run-as r & vs]]  [(str "Defaults>" r) (clojure.string/join "" (flatten vs))]
-       [[:default & vs]]  ["Defaults" (clojure.string/join "" (flatten vs))]
-       [[:not vs]]  ["!" vs]
-       [[:identifier i & rs]]  [i rs]
-       [[:value i & rs]]  [i rs]
-       [[:subtract f s]]  [f "-=" s]
+       [[:default/runas r vs]]  [(str "Defaults>" r) (clojure.string/join "," (flatten vs))]
+       [[:default & vs]]  ["Defaults" (clojure.string/join "," (flatten vs))]
+       [[:not i]]  (str "!" i)
+       [[:identifier i]]  i
+       [[:value [_ i]]] i
+       [[:subtract [i ve]]]  (str i "-=" ve)
        [[:equals f s]]  [f "=" s ","]
-       :else v)) ast))
+       :else  v)) ast))
 
 (comment
   (def cmnd-alias [:cmnd-alias "C_PIPES"
-                   [:file "/usr/bin/tee" :wildcard "*"]
-                   [:file "/usr/bin/tee" :flag "-a" :wildcard "*"]])
+                   [[:file "/usr/bin/tee"] [:wildcard "*"]]
+                   [[:file "/usr/bin/tee"] [:flag "-a"] [:wildcard "*"]]])
 
   (def user-spec [:user-spec
-                  [:user "re-ops" :hosts "ALL"]
-                  [:tag "NOPASSWD" :alias-name "C_SERVICE"]
-                  [:alias-name "C_KERNEL"]])
+                  [[:user "re-ops"]]
+                  [[:hosts "ALL"]]
+                  [[[:tag "NOPASSWD"] [:alias-name "C_SERVICE"]] [[:alias-name "C_KERNEL"]]]])
 
-  (def default-simple [:default [:identified "exempt_group" :value "re-ops"]])
+  (def default-simple [:default [[[:identifier "exempt_group"] [:value [:user "re-ops"]]]]])
 
-  (def subtract-default [:default [[:not [:identifier "env_reset"]] [:subtract [:identifier "env_delete" :value "PATH"]]]])
+  (def subtract-default [:default [[:not [:identifier "env_reset"]] [:subtract [[:identifier "env_delete"] [:value [:environment "PATH"]]]]]])
 
-  (def run-as-default [:default/run-as "root" [:not [:identified "set_logname"]]])
+  (def run-as-default [:default/runas "root" [[:not [:identifier "set_logname"]]]])
+
+  (emit subtract-default)
+  (emit run-as-default)
 
   (emit [:default [:equals [:identifier "foo"] [:value "PATH"]] [:not [:identifier "env_reset"]]])
   (emit [:default/run-as "root" [:equals [:identifier "foo"] [:value "PATH"]] [:not [:identifier "env_reset"]]]))
